ddff58a7451daa54915941985d9b0ab0dc3b02fd
lilei2345 <1795455954@qq.com>
2021-04-07 17:13:31 +0800

    第一次提交

---
+++ app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
@@ -0,0 +1,31 @@
+package com.huatec.hiot_cloud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.huatec.hiot_cloud.test.mvptest.model.User;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("tag", "test");
+
+    }
+
+    private void login(User user) {
+        if("lilei".equals(user.getUserName())&& "123".equals(user.getPassword())) {
+
+            Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "登录失败", Toast.LENGTH_SHORT).show();
+        }
+    }}
